beff0120dcd78a23c379cc6c830ff3d174eb6e14
Yoav <yoav@idomoo.com>
2018-11-17 12:54:05 +0100

    Improved Chromecast client Updated devices dialog Code cleanup and refactoring

--- app/src/main/java/yoavbz/googlehomeedge/DeviceDialog.java
+++ app/src/main/java/yoavbz/googlehomeedge/DeviceDialog.java
@@ -1,53 +1,171 @@
 package yoavbz.googlehomeedge;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.samsung.android.sdk.SsdkUnsupportedException;
+import com.samsung.android.sdk.look.Slook;
+import su.litvak.chromecast.api.v2.ChromeCast;
+import su.litvak.chromecast.api.v2.ChromeCasts;
+import su.litvak.chromecast.api.v2.ChromeCastsListener;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Set;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class DeviceDialog extends Activity {
 
 	private static final String TAG = DeviceDialog.class.getSimpleName();
+	DialogAdapter adapter;
+	ProgressBar progressBar;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		checkEdgeSupport();
+
 		getWindow().setBackgroundDrawable(new ColorDrawable(0));
 		setFinishOnTouchOutside(false);
 
-		final SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
-		Set<String> devices = pref.getStringSet("devices", Collections.<String>emptySet());
-		final String[] names = new String[devices.size()], ids = new String[devices.size()];
-		int i = 0;
-		for (String device : devices) {
-			names[i] = device.substring(0, device.indexOf('_'));
-			ids[i++] = device.substring(device.indexOf('_') + 1);
-		}
-		Log.d(TAG, "Devices names: " + Arrays.toString(names) + ", ids: " + Arrays.toString(ids));
+		final AlertDialog.Builder builder = new AlertDialog.Builder(DeviceDialog.this);
+		adapter = new DialogAdapter(this, new ArrayList<ChromeCast>());
+		adapter.setNotifyOnChange(true);
+		View titleView = getLayoutInflater().inflate(R.layout.dialog_title, null);
+		progressBar = titleView.findViewById(R.id.device_progress_bar);
+		builder.setTitle("Select Device")
+		       .setIcon(R.drawable.google_home)
+		       .setOnDismissListener(new DialogInterface.OnDismissListener() {
+			       @Override
+			       public void onDismiss(DialogInterface dialog) {
+				       discoveryThread.interrupt();
+				       finish();
+			       }
+		       })
+		       .setAdapter(adapter, new DialogInterface.OnClickListener() {
+			       public void onClick(DialogInterface dialog, int which) {
+				       ChromeCast deviceSelected = adapter.getItem(which);
+				       Log.d(TAG, String.format("Selected device = %s", deviceSelected));
+				       SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(DeviceDialog.this);
+				       pref.edit()
+				           .putString("device", deviceSelected.getTitle())
+				           .putString("ip", deviceSelected.getAddress())
+				           .apply();
+				       dialog.dismiss();
+			       }
+		       })
+		       .setCustomTitle(titleView)
+		       .show();
+		discoveryThread.start();
+	}
 
-		AlertDialog.Builder builder = new AlertDialog.Builder(this);
-		builder.setTitle("Select Device").setItems(names, new DialogInterface.OnClickListener() {
-			public void onClick(DialogInterface dialog, int which) {
-				pref.edit().putString("deviceId", ids[which]).apply();
-				pref.edit().putString("deviceName", names[which]).apply();
+	Thread discoveryThread = new Thread(new Runnable() {
+		@Override
+		public void run() {
+			try {
+				Log.d(TAG, "Starting Chromecast device discovery");
+				ChromeCasts.registerListener(addDeviceToDialogListener);
+				ChromeCasts.startDiscovery(InetAddress.getLocalHost());
+				TimeUnit.SECONDS.sleep(30);
+				stopDiscovery();
+				progressBar.setVisibility(View.INVISIBLE);
+			} catch (IOException e) {
+				Log.e(TAG, e.getMessage());
+			} catch (InterruptedException e) {
+				Log.d(TAG, "Device selection interrupted discoveryThread");
+				stopDiscovery();
 			}
-		}).setOnDismissListener(new DialogInterface.OnDismissListener() {
-			@Override
-			public void onDismiss(DialogInterface dialog) {
-				Intent intent = new Intent(DeviceDialog.this, PanelProvider.class);
-				intent.setAction("yoavbz.googlehomeedge.action.ACTION_REFRESH");
-				sendBroadcast(intent);
+		}
+	});
+
+	private void stopDiscovery() {
+		try {
+			Log.d(TAG, "Stopping Chromecast device discovery");
+			ChromeCasts.stopDiscovery();
+		} catch (IOException e) {
+			Log.e(TAG, e.getMessage());
+		}
+	}
+
+	ChromeCastsListener addDeviceToDialogListener = new ChromeCastsListener() {
+		int index = 0;
+
+		@Override
+		public void newChromeCastDiscovered(final ChromeCast chromeCast) {
+			runOnUiThread(new Runnable() {
+				@Override
+				public void run() {
+					Log.d(TAG, String.format("Found device #%d = %s", index++, chromeCast));
+					adapter.add(chromeCast);
+				}
+			});
+		}
+
+		@Override
+		public void chromeCastRemoved(ChromeCast chromeCast) {
+		}
+	};
+
+	private void checkEdgeSupport() {
+		Slook slook = new Slook();
+		try {
+			slook.initialize(this);
+			if (!slook.isFeatureEnabled(Slook.COCKTAIL_PANEL)) {
+				Toast.makeText(this, getString(R.string.no_support), Toast.LENGTH_LONG).show();
 				finish();
 			}
-		}).show();
+		} catch (SsdkUnsupportedException e) {
+			Toast.makeText(this, getString(R.string.no_support), Toast.LENGTH_LONG).show();
+			finish();
+		}
+	}
+
+	private class DialogAdapter extends ArrayAdapter<ChromeCast> {
+		private Context mContext;
+		private List<ChromeCast> chromeCasts;
+
+		DialogAdapter(@NonNull Context context, ArrayList<ChromeCast> list) {
+			super(context, 0, list);
+			mContext = context;
+			chromeCasts = list;
+		}
+
+		@Nullable
+		@Override
+		public ChromeCast getItem(int position) {
+			return chromeCasts.get(position);
+		}
+
+		@NonNull
+		@Override
+		public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+			View listItem = convertView;
+			if (listItem == null) {
+				listItem = LayoutInflater.from(mContext).inflate(R.layout.list_item, parent, false);
+			}
+
+			ChromeCast currentDevice = chromeCasts.get(position);
+
+			TextView title = listItem.findViewById(R.id.device_title);
+			title.setText(currentDevice.getTitle());
+
+			return listItem;
+		}
 	}
 }
--- app/src/main/java/yoavbz/googlehomeedge/PanelProvider.java
+++ app/src/main/java/yoavbz/googlehomeedge/PanelProvider.java
@@ -4,38 +4,31 @@ import android.app.PendingIntent;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
-import android.util.ArraySet;
 import android.util.Log;
 import android.view.View;
 import android.widget.RemoteViews;
 import com.samsung.android.sdk.look.cocktailbar.SlookCocktailManager;
 import com.samsung.android.sdk.look.cocktailbar.SlookCocktailProvider;
-import okhttp3.*;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.IOException;
-import java.util.concurrent.TimeUnit;
+import su.litvak.chromecast.api.v2.ChromeCast;
 
 public class PanelProvider extends SlookCocktailProvider {
 
 	private static final String TAG = PanelProvider.class.getSimpleName();
-
 	private static final String ACTION_PLUS = "yoavbz.googlehomeedge.action.ACTION_PLUS";
 	private static final String ACTION_MINUS = "yoavbz.googlehomeedge.action.ACTION_MINUS";
-	private static final String ACTION_REFRESH = "yoavbz.googlehomeedge.action.ACTION_REFRESH";
+	private static final String ACTION_VOLUME_DIALOG = "yoavbz.googlehomeedge.action.ACTION_VOLUME_DIALOG";
+	static final String ACTION_REFRESH = "yoavbz.googlehomeedge.action.ACTION_REFRESH";
 	private RemoteViews panelRv = null;
 	private RemoteViews stateRv = null;
-	private OkHttpClient client = new OkHttpClient();
-	private static int volume;
+	private static Float volume;
 	private static boolean panelDisabled = true;
+	private ChromeCast chromeCast;
 
 	@Override
 	public void onEnabled(Context context) {
 		super.onEnabled(context);
+		updatePanelState(context);
 		fetchVolume(context);
 	}
 
@@ -47,16 +40,20 @@ public class PanelProvider extends SlookCocktailProvider {
 
 		switch (action) {
 			case ACTION_PLUS:
-				changeVolume(context, 5);
+				changeVolume(context, 0.05f);
 				break;
 			case ACTION_MINUS:
-				changeVolume(context, -5);
+				changeVolume(context, -0.05f);
+				break;
+			case ACTION_VOLUME_DIALOG:
+				Intent volumeIntent = new Intent(context, VolumeDialog.class);
+				volumeIntent.putExtra("volume", (int) (volume * 100));
+				context.startActivity(volumeIntent);
 				break;
 			case ACTION_REFRESH:
 				updatePanelState(context);
 				animateRefresh(context, true);
 				fetchVolume(context);
-				updateDeviceList(context);
 		}
 	}
 
@@ -64,106 +61,61 @@ public class PanelProvider extends SlookCocktailProvider {
 		stateRv.setViewVisibility(R.id.refresh_button, start ? View.GONE : View.VISIBLE);
 		stateRv.setViewVisibility(R.id.progress_bar, start ? View.VISIBLE : View.GONE);
 		SlookCocktailManager manager = SlookCocktailManager.getInstance(context);
-		int[] ids = manager.getCocktailIds(new ComponentName(context, getClass()));
+		int[] ids = manager.getCocktailIds(new ComponentName(context, PanelProvider.class));
 		manager.updateCocktail(ids[0], panelRv, stateRv);
 	}
 
-	private void updateDeviceList(final Context context) {
-		final SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(context);
-		String url = pref.getString("url", null);
-
-		// Fetching current devices names and ids
-		final Request request = new Request.Builder().url(url + "/device").build();
-		client.newCall(request).enqueue(new Callback() {
-			@Override
-			public void onFailure(Call call, IOException e) {
-				Log.e(TAG, e.toString());
-				animateRefresh(context, false);
-			}
-
-			@Override
-			public void onResponse(Call call, Response response) throws IOException {
-				try {
-					JSONArray arr = new JSONArray(response.body().string());
-					ArraySet<String> devices = new ArraySet<>();
-					for (int i = 0; i < arr.length(); i++) {
-						JSONObject o = arr.getJSONObject(i);
-						devices.add(String.format("%s_%s", o.getString("name"), o.getString("id")));
-					}
-					// Storing current devices list
-					pref.edit().putStringSet("devices", devices).apply();
-					TimeUnit.SECONDS.sleep(2); // For better animation
-					animateRefresh(context, false);
-				} catch (InterruptedException | JSONException e) {
-					Log.e(TAG, e.toString());
-				}
-			}
-		});
-	}
-
 	private void fetchVolume(final Context context) {
-		String url = PreferenceManager.getDefaultSharedPreferences(context).getString("url", null) + "/device";
-
-		final Request request = new Request.Builder().url(url).build();
-		client.newCall(request).enqueue(new Callback() {
+		new Thread(new Runnable() {
 			@Override
-			public void onFailure(Call call, IOException e) {
-				Log.d(TAG, "fetchVolume() : exception - " + e.toString());
-				panelDisabled = true;
-				updatePanelState(context);
-			}
-
-			@Override
-			public void onResponse(Call call, Response response) {
+			public void run() {
 				try {
-					String deviceId = PreferenceManager.getDefaultSharedPreferences(context)
-							.getString("deviceId", null);
-					JSONArray arr = new JSONArray(response.body().string());
-					for (int i = 0; i < arr.length(); i++) {
-						if (arr.getJSONObject(i).getString("id").equals(deviceId)) {
-							volume = arr.getJSONObject(i).getJSONObject("status").getInt("volume");
-							break;
-						}
+					String ip = PreferenceManager.getDefaultSharedPreferences(context).getString("ip", null);
+					if (chromeCast == null) {
+						chromeCast = new ChromeCast(ip);
 					}
+					Log.d(TAG, "Connecting to Chromecast address: " + chromeCast.getAddress());
+					chromeCast.connect();
+					volume = chromeCast.getStatus().volume.level;
 					panelDisabled = false;
 					updatePanelState(context);
+					animateRefresh(context, false);
+//					Log.d(TAG, "Disconnecting from Chromecast device: " + chromeCast);
+//					chromeCast.disconnect();
 				} catch (Exception e) {
-					Log.d(TAG, "fetchVolume() : exception - " + e.toString());
+					Log.e(TAG, "fetchVolume() : exception - " + e.toString());
+					panelDisabled = true;
+					updatePanelState(context);
+					animateRefresh(context, false);
 				}
 			}
-		});
+		}).start();
 	}
 
-	private void changeVolume(final Context context, final int amount) {
-		String url = PreferenceManager.getDefaultSharedPreferences(context).getString("url", null);
-		if (url == null) {
-			panelDisabled = false;
-			updatePanelState(context);
-			return;
-		}
-		String id = PreferenceManager.getDefaultSharedPreferences(context).getString("deviceId", null);
-		url = String.format("%s/device/%s/volume/%d", url, id, volume += amount);
-
-		Log.d(TAG, "changeVolume() : url - " + url);
-		Request request = new Request.Builder().url(url).build();
-		client.newCall(request).enqueue(new Callback() {
+	private void changeVolume(final Context context, final float amount) {
+		new Thread(new Runnable() {
 			@Override
-			public void onFailure(Call call, IOException e) {
-				Log.d(TAG, e.getMessage());
-				volume -= amount;
-				panelDisabled = true;
-				updatePanelState(context);
+			public void run() {
+				try {
+					volume = Math.min(100, Math.max(0, volume += amount));
+					String ip = PreferenceManager.getDefaultSharedPreferences(context).getString("ip", null);
+					if (chromeCast == null) {
+						chromeCast = new ChromeCast(ip);
+					}
+					chromeCast.connect();
+					chromeCast.setVolume(volume);
+					Log.d(TAG, String.format("Added amount: %.2f, to current volume: %.2f", amount, volume));
+					panelDisabled = false;
+					updatePanelState(context);
+//					chromeCast.disconnect();
+				} catch (Exception e) {
+					Log.e(TAG, e.getMessage());
+					volume -= amount;
+					panelDisabled = true;
+					updatePanelState(context);
+				}
 			}
-
-			@Override
-			public void onResponse(Call call, Response response) {
-				// Limit volume to values between 0-100
-				volume = Math.min(100, Math.max(0, volume));
-				Log.d(TAG, String.format("Added amount: %d, to current volume: %d", volume, amount));
-				panelDisabled = false;
-				updatePanelState(context);
-			}
-		});
+		}).start();
 	}
 
 	@Override
@@ -177,19 +129,24 @@ public class PanelProvider extends SlookCocktailProvider {
 	private RemoteViews createPanelView(Context context) {
 		RemoteViews panelView = new RemoteViews(context.getPackageName(), R.layout.main_panel);
 
-		Intent plusIntent = new Intent(context, getClass());
+		Intent plusIntent = new Intent(context, PanelProvider.class);
 		plusIntent.setAction(ACTION_PLUS);
-		Intent minusIntent = new Intent(context, getClass());
+		Intent minusIntent = new Intent(context, PanelProvider.class);
 		minusIntent.setAction(ACTION_MINUS);
+		Intent volumeInputIntent = new Intent(context, PanelProvider.class);
+		volumeInputIntent.setAction(ACTION_VOLUME_DIALOG);
 		Intent dialogIntent = new Intent(context, DeviceDialog.class);
 		dialogIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 
 		PendingIntent plusPendingIntent = PendingIntent.getBroadcast(context, 0, plusIntent, PendingIntent.FLAG_UPDATE_CURRENT);
 		PendingIntent minusPendingIntent = PendingIntent.getBroadcast(context, 0, minusIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+		PendingIntent volumeInputPendingIntent = PendingIntent.getBroadcast(context, 0, volumeInputIntent,
+		                                                                    PendingIntent.FLAG_UPDATE_CURRENT);
 		PendingIntent dialogPendingIntent = PendingIntent.getActivity(context, 0, dialogIntent, PendingIntent.FLAG_UPDATE_CURRENT);
 
 		panelView.setOnClickPendingIntent(R.id.plus, plusPendingIntent);
 		panelView.setOnClickPendingIntent(R.id.minus, minusPendingIntent);
+		panelView.setOnClickPendingIntent(R.id.volume_text, volumeInputPendingIntent);
 		panelView.setOnClickPendingIntent(R.id.device_select, dialogPendingIntent);
 		panelView.setImageViewResource(R.id.logo, R.drawable.google_home);
 
@@ -198,24 +155,18 @@ public class PanelProvider extends SlookCocktailProvider {
 
 	private RemoteViews createStateView(Context context) {
 		RemoteViews stateView = new RemoteViews(context.getPackageName(), R.layout.state_layout);
-
-		Intent refreshIntent = new Intent(context, getClass());
+		Intent refreshIntent = new Intent(context, PanelProvider.class);
 		refreshIntent.setAction(ACTION_REFRESH);
 		PendingIntent refreshPendingIntent = PendingIntent.getBroadcast(context, 0, refreshIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-
 		stateView.setOnClickPendingIntent(R.id.refresh_button, refreshPendingIntent);
 		return stateView;
 	}
 
 	private void updatePanelState(Context context) {
 		SlookCocktailManager cocktailManager = SlookCocktailManager.getInstance(context);
-		int[] cocktailIds = cocktailManager.getCocktailIds(new ComponentName(context, getClass()));
-		if (panelRv == null) {
-			panelRv = createPanelView(context);
-		}
-		if (stateRv == null) {
-			stateRv = createStateView(context);
-		}
+		int[] cocktailIds = cocktailManager.getCocktailIds(new ComponentName(context, PanelProvider.class));
+		panelRv = (panelRv != null) ? panelRv : createPanelView(context);
+		stateRv = (stateRv != null) ? stateRv : createStateView(context);
 		if (panelDisabled) {
 			panelRv.setBoolean(R.id.plus, "setEnabled", false);
 			panelRv.setBoolean(R.id.minus, "setEnabled", false);
@@ -227,13 +178,12 @@ public class PanelProvider extends SlookCocktailProvider {
 			panelRv.setBoolean(R.id.minus, "setEnabled", true);
 			panelRv.setTextViewText(R.id.volume_title, "Volume: ");
 			panelRv.setViewVisibility(R.id.volume_text, View.VISIBLE);
-			panelRv.setTextViewText(R.id.volume_text, Integer.toString(volume));
+			panelRv.setTextViewText(R.id.volume_text, Integer.toString((int) (volume * 100)));
 			panelRv.setViewVisibility(R.id.device_select, View.VISIBLE);
 			String selectedDevice = PreferenceManager.getDefaultSharedPreferences(context)
-					.getString("deviceName", "Select device");
+			                                         .getString("device", "Select device");
 			panelRv.setTextViewText(R.id.device_select, selectedDevice);
 		}
-
 		if (cocktailIds.length != 0) {
 			cocktailManager.updateCocktail(cocktailIds[0], panelRv, stateRv);
 		}
